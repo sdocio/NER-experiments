50962b44c528883f39dfb2994ff67dd607925fe2
Susana Sotelo <sdocio@users.noreply.github.com>
2023-05-07 02:07:32 +0200

    Minor fix in documentation
    - Updated documentation (minor fix)
    - Refactoring of predict_spacy.py

--- 1-spacy/README.md
+++ 1-spacy/README.md
@@ -3,7 +3,7 @@ Config files used to train the [spaCy](https://spacy.io/) models for the NER tas
 | folder | type                        | pipeline | trained model |
 | ------ | --------------------------- | ---------------------------------------------------- | ----- |
 | `cnn`  | [CNNs and Bloom embeddings](https://spacy.io/universe/project/video-spacys-ner-model) | `tok2vec` (`es_core_news_lg`) | [es_spacy_ner_cds](https://huggingface.co/sdocio/es_spacy_ner_cds)
-| `trf`    | transformers              | `transformer` (`es_dep_news_trf`)    | [es_spacy_ner_cds_trf](https://huggingface.co/sdocio/es_spacy_ner_cds_trf)
+| `trf`    | transformers              | `transformer` (`dccuchile/bert-base-spanish-wwm-cased`)    | [es_spacy_ner_cds_trf](https://huggingface.co/sdocio/es_spacy_ner_cds_trf)
 
 In both cases the dataset was converted from IOB2 using spaCy utilities and trained with `run_train.sh`.
 
@@ -15,26 +15,12 @@ $ python -m spacy debug data config.cfg
 $ bash run_train.sh
 ```
 
-Models can be evaluated using `predict_spacy.py` and a testing dataset in IOB2 format. It produces the resulting predictions in IOB format and the evaluation metrics.
+The script `predict_spacy.py` produces the resulting predictions in IOB format, which can be evaluated using `eval.py` and a testing dataset in IOB2 format.
 
 ```bash
 $ python3 predict_spacy.py -m cnn/model/es_spacy_ner_cds ../datasets/test.iob > predictions.iob
 100%|██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 15178/15178 [03:52<00:00, 65.37it/s]
-              precision    recall  f1-score   support
-
-       B-LOC      0.977     0.989     0.983      3626
-       I-LOC      0.926     0.944     0.935       808
-      B-MISC      0.889     0.788     0.835       325
-      I-MISC      0.777     0.721     0.748       280
-       B-ORG      0.885     0.701     0.783        77
-       I-ORG      0.876     0.842     0.859       101
-       B-PER      0.946     0.895     0.920       237
-       I-PER      0.954     0.856     0.902        97
-
-   micro avg      0.951     0.944     0.947      5551
-   macro avg      0.904     0.842     0.871      5551
-weighted avg      0.949     0.944     0.946      5551
-
+$ python ../utils/eval.py predictions.iob ../datasets/test.iob
               precision    recall  f1-score   support
 
          LOC      0.975     0.987     0.981      3626
--- 1-spacy/predict_spacy.py
+++ 1-spacy/predict_spacy.py
@@ -2,26 +2,19 @@ import argparse
 import spacy
 import sys
 import warnings
-from itertools import chain
-from sklearn.metrics import classification_report
-from seqeval.metrics import classification_report as seq_classification_report
 from tqdm import tqdm
 
 warnings.filterwarnings("ignore")
-TAGS = [
-    'B-LOC', 'I-LOC', 'B-MISC', 'I-MISC', 'B-ORG', 'I-ORG', 'B-PER', 'I-PER'
-]
 
 
 class IOB:
-    def __init__(self, ifile, sep=" "):
-        self._ifile = ifile
+    def __init__(self, sep=" "):
         self._sep = sep
 
-    def convert_file(self):
+    def parse_file(self, ifile):
         return [
             self._parse_sentence(raw)
-            for raw in self._read_sentences_from_file(self._ifile)
+            for raw in self._read_sentences_from_file(ifile)
         ]
 
     def _parse_sentence(self, raw_sentence):
@@ -51,12 +44,12 @@ class IOB:
             print(err, file=sys.stderr)
             sys.exit()
 
-
-def token_to_iob(token):
-    return (
-        token.ent_iob_ if token.ent_iob_ == 'O'
-        else f'{token.ent_iob_}-{token.ent_type_}'
-    )
+    @classmethod
+    def token_to_iob(cls, token):
+        return (
+            token.ent_iob_ if token.ent_iob_ == 'O'
+            else f'{token.ent_iob_}-{token.ent_type_}'
+        )
 
 
 def parse_args():
@@ -79,52 +72,26 @@ def parse_args():
         metavar='MODEL',
         help='model'
     )
-    parser.add_argument(
-        '-e',
-        '--eval',
-        action='store_true',
-        default=False,
-        help='Evaluation only'
-    )
 
     return parser.parse_args()
 
 
-def flatten(y):
-    return list(chain.from_iterable(y))
-
-
 args = parse_args()
+iob = IOB()
 
 nlp = spacy.load(args.model)
 sentences = [
-    [token for token in sent] for sent in IOB(args.dataset).convert_file()
+    [token[0] for token in sent] for sent in iob.parse_file(args.dataset)
 ]
-golden = [[token[-1] for token in sent] for sent in sentences]
-labels = sorted(
-    [label for label in TAGS if label != 'O'],
-    key=lambda name: (name[1:], name[0]))
 
 predict = []
-for sent in tqdm(sentences):
-    tokens = [token[0] for token in sent]
+for tokens in tqdm(sentences):
     predict.append([
-        (token.text, token_to_iob(token))
+        (token.text, IOB.token_to_iob(token))
         for token in nlp(spacy.tokens.Doc(nlp.vocab, tokens))
     ])
-y_pred = [[token[-1] for token in sent] for sent in predict]
 
-if not args.eval:
-    for sent in predict:
-        for token in sent:
-            print(" ".join(token))
-        print()
-
-results = classification_report(
-    flatten(golden), flatten(y_pred), labels=labels, digits=3)
-
-print(results, file=sys.stderr)
-print()
-
-results = seq_classification_report(golden, y_pred, digits=3)
-print(results, file=sys.stderr)
+for sent in predict:
+    for token in sent:
+        print(" ".join(token))
+    print()
