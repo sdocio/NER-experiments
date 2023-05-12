9629e53769a15103469d3a978a6765e508c6ec6a
Susana Sotelo <sdocio@users.noreply.github.com>
2023-05-12 15:18:26 +0200

    Minor modifications in eval.py
    - Added some docstrings
    - Added some typing annotations
    - Solved some pylint reported problems

--- utils/eval.py
+++ utils/eval.py
@@ -1,9 +1,12 @@
+"""
+Script that evaluates a IOB dataset using a gold standard
+"""
 import argparse
 import csv
 import sys
+from itertools import chain
 import matplotlib.pyplot as plt
 import seaborn as sns
-from itertools import chain
 from seqeval.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
@@ -13,25 +16,38 @@ TAGS = [
 
 
 class IOB:
-    def __init__(self, sep=" "):
+    """
+    Class to manage IOB files
+    """
+    def __init__(self, sep: str = " "):
         self._sep = sep
 
-    def parse_file(self, ifile):
+    def __repr__(self):
+        return f'IOB object with separation char = "{self._sep}"'
+
+    def parse_file(self, ifile: str) -> list:
+        '''
+        Takes a filename and returns a nested list with tuples containing
+        the tokens.
+
+        >>> iob.parse_file("../datasets/test.iob")
+        [[('Não', 'O'), ('sei', 'O'), ('.', 'O')], [('Não', 'O'), ('.', 'O')]]
+        '''
         return [
             self._parse_sentence(raw)
             for raw in self._read_sentences_from_file(ifile)
         ]
 
-    def _parse_sentence(self, raw_sentence):
+    def _parse_sentence(self, raw_sentence: str) -> list:
         return [
-            tuple(token.split(self._sep))
+            (token.split(self._sep))
             for token in raw_sentence.strip().split("\n")
         ]
 
-    def _read_sentences_from_file(self, ifile):
+    def _read_sentences_from_file(self, ifile: str):
         raw_sentence = ""
         try:
-            with open(ifile) as fhi:
+            with open(ifile, encoding="utf-8") as fhi:
                 for line in fhi:
                     if line == "\n":
                         if raw_sentence == "":
@@ -50,7 +66,8 @@ class IOB:
             sys.exit()
 
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
+    """Parses script arguments"""
     description = "Eval model results comparing two IOB files."
 
     parser = argparse.ArgumentParser(
@@ -92,31 +109,43 @@ def parse_args():
     return parser.parse_args()
 
 
-def check(data1, data2):
+def check(data1: list, data2: list) -> None:
+    """
+    Takes two nested lists and cheks if their dimensions are the same
+    """
     assert len(data1) == len(data2), (
         "Different number of sentences between datasets"
     )
-    for i in range(len(data1)):
+    for i, _ in enumerate(data1):
         assert len(data1[i]) == len(data2[i]), (
             f"Different number of tokens in line {i+1}")
 
 
-def fix(data1, data2):
+def fix(data1, data2) -> None:
+    """Removes inconsistent entries (with different lengths) in the datasets"""
     skip = 0
-    for i in range(len(data1)-1, -1, -1):
-        if len(data1[i]) != len(data2[i]):
-            del data1[i]
-            del data2[i]
-            skip += 1
+    try:
+        for i in range(len(data1)-1, -1, -1):
+            if len(data1[i]) != len(data2[i]):
+                del data1[i]
+                del data2[i]
+                skip += 1
+    except IndexError as exc:
+        raise IndexError("datasets with different sizes") from exc
     if skip > 0:
         print(f"Skipped {skip} inconsistent sentences.", file=sys.stderr)
 
 
-def flatten(y):
-    return list(chain.from_iterable(y))
+def flatten(nested_dataset: list) -> list:
+    """Takes a nested list and returns it unnested"""
+    return list(chain.from_iterable(nested_dataset))
 
 
-def convert_csv(results):
+def convert_csv(results: dict) -> None:
+    """
+    Writes a CSV file using the dictionary returned by
+    seqeval.classification_report() with the evaluation results
+    """
     ofile = csv.writer(
         sys.stdout,
         delimiter=',',
@@ -139,45 +168,49 @@ def convert_csv(results):
             ])
 
 
-args = parse_args()
-iob = IOB()
+def evaluate() -> None:
+    """
+    Takes a dataset in IOB2 format and evaluates it against a gold standard
+    """
+    args = parse_args()
+    iob = IOB()
 
-golden_tags = [
-    [token[-1] for token in sent] for sent in iob.parse_file(args.golden)
-]
-dataset_tags = [
-    [token[-1] for token in sent] for sent in iob.parse_file(args.dataset)
-]
+    golden_tags = [
+        [token[-1] for token in sent] for sent in iob.parse_file(args.golden)
+    ]
+    dataset_tags = [
+        [token[-1] for token in sent] for sent in iob.parse_file(args.dataset)
+    ]
 
-if args.strict is False:
-    fix(golden_tags, dataset_tags)
-check(golden_tags, dataset_tags)
+    if args.strict is False:
+        fix(golden_tags, dataset_tags)
+    check(golden_tags, dataset_tags)
 
-labels = sorted(
-    [label for label in TAGS],
-    key=lambda name: (name[1:], name[0]))
+    if args.output == 'csv':
+        eval_results = classification_report(
+            golden_tags,
+            dataset_tags,
+            digits=3,
+            output_dict=True)
+        convert_csv(eval_results)
+    elif args.output == 'plot':
+        cmatrix = confusion_matrix(
+            flatten(golden_tags), flatten(dataset_tags), normalize="true")
+        plt.figure(figsize=(8, 5))
+        plot = sns.heatmap(cmatrix, annot=True, fmt=".3f", cmap="GnBu")
+        plot.set_xlabel('Predicted Values')
+        plot.set_ylabel('Actual Values')
+        plot.xaxis.set_ticklabels(TAGS + ["O"])
+        plot.yaxis.set_ticklabels(TAGS + ["O"])
+        plot.tick_params(axis="y", rotation=20)
+        plt.savefig(args.plot_file, format="pdf", bbox_inches="tight")
+    else:
+        eval_results = classification_report(
+            golden_tags,
+            dataset_tags,
+            digits=3)
+        print(eval_results)
 
-if args.output == 'csv':
-    results = classification_report(
-        golden_tags,
-        dataset_tags,
-        digits=3,
-        output_dict=True)
-    convert_csv(results)
-elif args.output == 'plot':
-    cm = confusion_matrix(
-        flatten(golden_tags), flatten(dataset_tags), normalize="true")
-    plt.figure(figsize=(8, 5))
-    fx = sns.heatmap(cm, annot=True, fmt=".3f", cmap="GnBu")
-    fx.set_xlabel('Predicted Values')
-    fx.set_ylabel('Actual Values')
-    fx.xaxis.set_ticklabels(labels + ["O"])
-    fx.yaxis.set_ticklabels(labels + ["O"])
-    fx.tick_params(axis="y", rotation=20)
-    plt.savefig(args.plot_file, format="pdf", bbox_inches="tight")
-else:
-    results = classification_report(
-        golden_tags,
-        dataset_tags,
-        digits=3)
-    print(results)
+
+if __name__ == "__main__":
+    evaluate()
