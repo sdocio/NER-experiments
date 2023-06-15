9b7108dea1b51953a850f87723aeba481834c5ea
Susana Sotelo <sdocio@users.noreply.github.com>
2023-06-15 16:47:09 +0200

    Update config files in spaCy transformers
    Config files were updated to reflect experiments fine-tuning diverse
    models using spaCy.

--- 1-spacy/cnn/run_train.sh
+++ 1-spacy/cnn/run_train.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-BASEDIR="/path/to/NER-experiments/1-spacy"
-CONFIGFILE="${BASEDIR}/cnn/config.cfg"
-OUTPUT_DIR="${BASEDIR}/cnn/model"
+NAME="spacy-cnn"
+BASEDIR="/path/to/NER-experiments"
+UTILSDIR="${BASEDIR}/utils"
+TEST_FILE="${BASEDIR}/datasets/test.iob"
+CONFIGFILE="${BASEDIR}/1-spacy/cnn/config.cfg"
+OUTPUT_DIR="${BASEDIR}/1-spacy/cnn/model"
 GPU_ID=0
 
+sbatch <<EOT
+#!/bin/sh
 
+#SBATCH -J ${NAME}
+#SBATCH --mem=24gb
+#SBATCH --time=05:00:00
+#SBATCH --output=${NAME}_%j.log
+#SBATCH -e ${NAME}_%j_error.log
+#SBATCH --gres=gpu:A100_40:1
+
+echo "Starting training: ${NAME}"
 time python3 -m spacy train ${CONFIGFILE} --gpu-id ${GPU_ID} --output ${OUTPUT_DIR}
+
+echo "Producing predictions to results/${NAME}_predictions.txt"
+time python ../predict_spacy.py -m ${OUTPUT_DIR} -m ${OUTPUT_DIR}/model-best ${TEST_FILE} > results/${NAME}_predictions.txt
+
+echo "Saving evaluation results to results/results_${NAME}.csv"
+time python ${UTILSDIR}/eval.py -o csv results/${NAME}_predictions.txt ${TEST_FILE} > results/results_${NAME}.csv
+
+EOT
